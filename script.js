b1e067820f05461c23d48bff7c4e05bc679102ec
Abdullah Nafis Chowdhury <abdullahnafis256@gmail.com>
2025-02-25 17:51:13 +0600

    New files uploaded

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Lets write Js');
+// console.log('Lets write Js');
+
+async function main(){
+
+    let a = await fetch('http://127.0.0.1:5500/songs/');
+    let response = await a.text();
+    console.log(response);  
+}
+
+main();
