e610bb321c62cb46c0ec1f1eac5674d242bd209d
Abdullah Nafis Chowdhury <abdullahnafis256@gmail.com>
2025-02-27 16:46:19 +0600

    Files updated

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 // console.log('Lets write Js');
 
-async function main(){
+async function getSongs() {
 
     let a = await fetch('http://127.0.0.1:5500/songs/');
     let response = await a.text();
-    console.log(response);  
+    console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    console.log(as);
+    let songs = [];
+
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i];
+
+        if (element.href.endsWith(".mp3")) {
+
+            songs.push(element.href);
+        }
+    }
+    // console.log(songs);
+    return songs;
 }
 
-main();
+getSongs();
+
+async function Songs() {
+
+    let songs = await getSongs();
+    // console.log('Songs are as follows:');
+    console.log(songs);
+
+    //Play audio
+
+    var audio = new Audio(songs[2]);
+    // audio.play();
+
+}
+
+Songs();
