c766ce68d8560a49c034d002fbfa0cfedcc9b03e
Abdullah Nafis Chowdhury <abdullahnafis256@gmail.com>
2025-03-02 12:15:43 +0600

    New updates has been made

--- script.js
+++ script.js
@@ -16,30 +16,44 @@ async function getSongs() {
 
         if (element.href.endsWith(".mp3")) {
 
-            songs.push(element.href);
+            // Extract just the filename
+            let songName = element.href.split('/').pop();
+            songs.push(decodeURIComponent(songName)); // Decode special characters
         }
     }
     // console.log(songs);
     return songs;
 }
 
-function displaySongs(){
-
-    
-}
 
 getSongs();
 
 async function Songs() {
 
     let songs = await getSongs();
-    // console.log('Songs are as follows:');
+    console.log('Songs are as follows:');
     console.log(songs);
+
+    let songUL = document.querySelector('.songslist').getElementsByTagName('ul')[0];
+    for (const song of songs) {     
+
+        songUL.innerHTML = songUL.innerHTML + `<li> 
+        <div class="listsongs">
+                            <img class="invert" src="logos/playbar/queue_music.svg" alt="">
+
+                            <div class="song"> ${songs} </div>
+                            <div class="artist">Artist</div>
+
+                            <img class="invert" src="logos/playbar/play_arrow.svg" alt="">
+                        </div></li>`;
+        
+    };
 
     //Play audio
 
     var audio = new Audio(songs[2]);
     // audio.play();
+
 
 }
 
