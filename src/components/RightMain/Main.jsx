31a2b012820c2f458c3112cc2541c7c3cce69b98
Nafis <abdullahnafis256@gmail.com>
2025-09-25 15:55:08 +0600

    Spotify Clone website

---
+++ src/components/RightMain/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='h-[88vh] w-[64%] rounded-lg bg-[#121212] text-white'>
+
+        <h1>This is Main component</h1>
+      
+    </div>
+  )
+}
+
+export default Main
